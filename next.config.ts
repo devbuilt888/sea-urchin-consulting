d1346a3ffcdfcee99df6c4f741a05a487229e893
zero <juanbeas1@icloud.com>
2025-07-24 01:00:26 -0400

    vercel fix 2

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/landing-pages' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/landing-pages' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
